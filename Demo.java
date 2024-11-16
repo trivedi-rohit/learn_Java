9e0c36fd4599e418917d9cf7f4b250e0171d7b5c
trivedi-rohit <mr.trivedirohit@gmail.com>
2024-11-16 17:54:15 +0530

    Data Types

---
+++ Demo.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Demo {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		System.out.println("Size of byte is ::" +Byte.SIZE);
+		System.out.println("Min of byte is ::" +Byte.MIN_VALUE);
+		System.out.println("Max of byte is ::" +Byte.MAX_VALUE);
+		System.out.println("Size of int is ::" +Integer.SIZE);
+		System.out.println("Max of int is ::" +Integer.MAX_VALUE);
+		System.out.println("Size of float is ::" +Float.SIZE);
+		System.out.println("Size of double is ::" +Double.SIZE);
+		System.out.println("Max value of long is :: "+Long.MAX_VALUE);
+		System.out.println("Min value of long is :: "+Long.MIN_VALUE);
+		
+	}
+
+}
