beee98578d17ee4a18d7fa2f05190901445fb7c3
trivedi-rohit <mr.trivedirohit@gmail.com>
2025-01-10 22:40:41 +0530

    update in pattern programs

---
+++ WWE.java
@@ -0,0 +1,173 @@
+/*public class WWE {
+
+	public static void main(String[] args) {
+		char c ='Z';
+		long l = 100_00l;
+		int i =92;
+		float f =2.02f;
+		double d =10_0.35d;
+		l = c+i;
+		f=c*l*i*f;
+		f=l+i+c;
+		i=(int)d;
+		f=(long)d;
+		System.out.print(f);
+	}}
+*/
+//========================================================================
+/*
+public class WWE {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int var = --a * a++ + a-- - --a;
+		System.out.println("a="+a);
+		System.out.print("var="+var);
+	}
+		
+}
+*/
+//========================================================================
+/*
+class WWE{
+	public static void main (String[]args) {
+		int i = 5;
+		if(i++<6)
+		{
+			System.out.println(i++);
+		}
+	}
+}
+*/
+//=========================================================================
+/*
+class WWE{
+	public static void main (String[]args) {
+		int x = 4;
+		int y =++(++x);
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
+*/
+//=========================================================================
+/*
+class WWE{
+	public static void main(String[] args){
+		byte c=(10>20)?30:40;
+		byte d=(10<20)?30:40;
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
+*/
+//==========================================================================
+
+/*
+class WWE{
+	public static void main (String[] args) {
+	int x = 5__0;
+	//int y = __50;
+	//int z = 50__;
+	float f = 123.76_86f;
+	double d = 1_2_3_4;
+	System.out.println(f);
+}
+}
+*/
+//=========================================================================
+// Pattern - I
+/*  
+ class WWE{
+	public static void main(String[]a) {
+		int n=10;
+		for (int i=0;i<n;i++)
+		{
+			for (int j=0;j<n;j++)
+			{
+				if (i==0&&j<n-1||j==(n-1)/2||i==(n-1)&&j<n-1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+		System.out.println();
+		}
+	}
+	
+}
+*/
+//======================================================================
+/*
+class WWE{
+	public static void main(String[]a) {
+		int n=10;
+		for (int i=0;i<n;i++)
+		{
+			for (int j=0;j<n;j++)
+			{
+				if (i==0&&j<n-1||j==(n-1)/2||i==(n-1)&&j<n-1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+		System.out.println();
+		}
+	}
+	
+}
+*/
+//=====================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- work.java
+++ work.java
@@ -1,12 +1,22 @@
-package work;
-
 public class work {
 
 	public static void main(String[] args) {
-		double a = 45;
-		byte b;
-		b=(byte)a;
-		System.out.println(b);
+		// While loop concept
+//		int r = 5;
+//		int c = 0;
+//		while (c<r)
+//		{
+//			System.out.println("*");
+//			c++;
+//		}
+		// do-while loop concept
+		
+		int n = 5;
+		int i = 0;
+		do {
+			System.out.println("*");
+		}while(i<n);
+				
 	}
 
 }
